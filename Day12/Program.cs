eed328fb6145528dd4623ef50a4a18eea1bb0db6
Jan Eric Traut <traut.j@t-online.de>
2024-12-19 15:36:15 +0100

    fix donat shapes

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,5 +1,4 @@
-﻿
-string file = Environment.CurrentDirectory + "/input.txt";
+﻿string file = Environment.CurrentDirectory + "/input.txt";
 //string file = Environment.CurrentDirectory + "/example.txt";
 List<string> lines = File.ReadAllText(file).Replace("\r", "").Split("\n").ToList();
 
@@ -12,14 +11,14 @@ for(int i = 0; i < lines.Count; i++)
     gardenSearched[i] = garden[i].ToArray();
 }
 
-int maxX = garden.Length;
-int maxY = garden[0].Length;
+int sizeX = garden.Length;
+int sizeY = garden[0].Length;
 
 List<Dictionary<(int, int), char>> plantGroups = [];
 
-for(int x = 0; x < maxX; x++)
+for(int x = 0; x < sizeX; x++)
 {
-    for(int y = 0; y < maxY; y++)
+    for(int y = 0; y < sizeY; y++)
     {
         if(gardenSearched[x][y] == '0')
         {
@@ -37,11 +36,10 @@ foreach(var plantField in plantGroups)
     int area = 0;
     int perimeter = 0;
     var value = plantField.First().Value;
-    int x, y;
     foreach(var plant in plantField)
     {
-        x = plant.Key.Item1;
-        y = plant.Key.Item2;
+        int x = plant.Key.Item1;
+        int y = plant.Key.Item2;
         area++;
 
         if(x - 1 < 0 || garden[x - 1][y] != value)
@@ -52,160 +50,106 @@ foreach(var plantField in plantGroups)
         {
             perimeter++;
         }
-        if(x + 1 >= maxX || garden[x + 1][y] != value)
+        if(x + 1 >= sizeX || garden[x + 1][y] != value)
         {
             perimeter++;
         }
-        if(y + 1 >= maxY || garden[x][y + 1] != value)
+        if(y + 1 >= sizeY || garden[x][y + 1] != value)
         {
             perimeter++;
         }
     }
-    (x, y) = plantField.First().Key;
-    int startX = x;
-    int startY = y;
-    Direction direction = Direction.Right;
-    Direction newDirection;
-    bool finshed = false;
-    int sides = 1;
-    while(!finshed)
+    int sides = 0;
+
+    int minX = plantField.Keys.Min(x => x.Item1);
+    int maxX = plantField.Keys.Max(x => x.Item1);
+    int minY = plantField.Keys.Min(x => x.Item2);
+    int maxY = plantField.Keys.Max(x => x.Item2);
+
+    for(int x = minX; x <= maxX; x++)
     {
-        switch(direction)
+        bool top = false;
+        bool bottom = false;
+
+        List<(int, int)> row = plantField.Where(p => p.Key.Item1 == x).Select(p => p.Key).ToList();
+        if(x == minX)
         {
-            case Direction.Up:
-                if(y - 1 >= 0 && garden[x][y - 1] == value)
-                {
-                    y--;
-                    sides++;
-                    direction = Direction.Left;
-                }
-                else if(x - 1 >= 0 && garden[x - 1][y] == value)
-                {
-                    x--;
-                }
-                else if(y + 1 < maxY && garden[x][y + 1] == value)
-                {
-                    y++;
-                    sides++;
-                    direction = Direction.Right;
-                }
-                else
-                {
-                    sides++;
-                    direction = Direction.Right;
-                }
-                break;
-            case Direction.Right:
-                if(x - 1 >= 0 && garden[x - 1][y] == value)
-                {
-                    x--;
-                    sides++;
-                    direction = Direction.Up;
-                }
-                else if(y + 1 < maxY && garden[x][y + 1] == value)
-                {
-                    y++;
-                }
-                else if(x + 1 < maxX && garden[x + 1][y] == value)
-                {
-                    x++;
-                    sides++;
-                    direction = Direction.Down;
-                }
-                else
-                {
-                    sides++;
-                    direction = Direction.Down;
-                }
-                break;
-            case Direction.Down:
-                if(y + 1 < maxY && garden[x][y + 1] == value)
-                {
-                    y++;
-                    sides++;
-                    direction = Direction.Right;
-                }
-                else if(x + 1 < maxX && garden[x + 1][y] == value)
-                {
-                    x++;
-                }
-                else if(y - 1 >= 0 && garden[x][y - 1] == value)
-                {
-                    y--;
-                    sides++;
-                    direction = Direction.Left;
-                }
-                else
-                {
-                    sides++;
-                    direction = Direction.Left;
-                }
-                break;
-            case Direction.Left:
-                if(x + 1 < maxX && garden[x + 1][y] == value)
-                {
-                    x++;
-                    sides++;
-                    direction = Direction.Down;
-                }
-                else if(y - 1 >= 0 && garden[x][y - 1] == value)
-                {
-                    y--;
-                }
-                else if(x - 1 >= 0 && garden[x - 1][y] == value)
-                {
-                    x--;
-                    sides++;
-                    direction = Direction.Up;
-                }
-                else
-                {
-                    sides++;
-                    direction = Direction.Up;
-                }
-                break;
-            default:
-                break;
-        }
-        if(x == startX && y == startY)
-        {
-            if(direction == Direction.Up)
+            for(int y = minY; y <= maxY; y++)
             {
-                finshed = true;
+                bool newBottom = row.Contains((x, y));
+
+                if(newBottom && newBottom != bottom)
+                {
+                    sides++;
+                }
+
+                bottom = newBottom;
             }
         }
+        bottom = false;
+        for(int y = minY; y <= maxY; y++)
+        {
+            bool newTop = row.Contains((x, y));
+            bool newBottom = x + 1 < sizeX && plantField.ContainsKey((x + 1, y));
+            if(newTop ^ newBottom)
+            {
+                if(top != newTop)
+                {
+                    sides++;
+                }
+                else if(bottom != newBottom)
+                {
+                    sides++;
+                }
+            }
+            top = newTop;
+            bottom = newBottom;
+        }
     }
-    Draw(garden, value);
-    var temp = area * perimeter;
-    price += temp;
-    temp = area * sides;
-    Console.WriteLine($"{value}: {area} {sides} = {temp}");
-    price2 += temp;
+    for(int y = minY; y <= maxY; y++)
+    {
+        bool left = false;
+        bool right = false;
+
+        List<(int, int)> column = plantField.Where(p => p.Key.Item2 == y).Select(p => p.Key).ToList();
+        if(y == minY)
+        {
+            for(int x = minX; x <= maxX; x++)
+            {
+                bool newRight = column.Contains((x, y));
+                if(newRight && newRight != right)
+                {
+                    sides++;
+                }
+                right = newRight;
+            }
+        }
+        right = false;
+        for(int x = minX; x <= maxX; x++)
+        {
+            bool newLeft = column.Contains((x, y));
+            bool newRight = y + 1 < sizeY && plantField.ContainsKey((x, y + 1));
+            if(newLeft ^ newRight)
+            {
+                if(left != newLeft)
+                {
+                    sides++;
+                }
+                else if(right != newRight)
+                {
+                    sides++;
+                }
+            }
+            left = newLeft;
+            right = newRight;
+        }
+    }
+    price += area * perimeter;
+    price2 += area * sides;
 }
 
 Console.WriteLine(price);
 Console.WriteLine(price2);
-
-void Draw(char[][] garden, char value)
-{
-    string output = "";
-    for(int i = 0; i < garden.Length; i++)
-    {
-        for(int j = 0; j < garden[i].Length; j++)
-        {
-            if(garden[i][j] == value)
-            {
-                output += value;
-            }
-            else
-            {
-                output += ".";
-            }
-        }
-        output += "\n";
-    }
-    File.WriteAllText(Environment.CurrentDirectory + "/output.txt", output);
-}
 
 void searchField(char plant, int x, int y)
 {
@@ -222,20 +166,12 @@ void searchField(char plant, int x, int y)
     {
         searchField(plant, x, y - 1);
     }
-    if(x + 1 < maxX && gardenSearched[x + 1][y] == plant)
+    if(x + 1 < sizeX && gardenSearched[x + 1][y] == plant)
     {
         searchField(plant, x + 1, y);
     }
-    if(y + 1 < maxY && gardenSearched[x][y + 1] == plant)
+    if(y + 1 < sizeY && gardenSearched[x][y + 1] == plant)
     {
         searchField(plant, x, y + 1);
     }
-}
-
-enum Direction
-{
-    Up,
-    Right,
-    Down,
-    Left,
 }
